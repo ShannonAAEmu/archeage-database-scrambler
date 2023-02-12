63332b56b88c287ecfb6c5cdf34b3ee852224a97
Shannon Max <jeyms_bont@mail.ru>
2023-02-12 18:48:29 +0300

    Console UI

--- src/main/java/info/aaemu/shannon/tools/service/FileManagerService.java
+++ src/main/java/info/aaemu/shannon/tools/service/FileManagerService.java
@@ -2,7 +2,9 @@ package info.aaemu.shannon.tools.service;
 
 public interface FileManagerService {
 
+    boolean isAvailableFile(String name);
+
     byte[] readFile(String name);
 
-    void writeFile(byte[] data, String fileName);
+    void writeFile(byte[] data, String name);
 }
